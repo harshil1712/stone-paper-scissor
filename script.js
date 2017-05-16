b58b393977be5b43c521f270ddee80604c73fc4b
ghagrawal17@gmail.com <ghagrawal17@gmail.com>
2017-05-17 01:52:31 +0530

    Fixing computer choice error

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	var userChoice,computerChoice,playerCount=0,computerCount=0;
+	var userChoice,computerChoice,playerCount=0,computerCount=0,computerChoice;
 	$('#stone').click(function(){
 		userChoice='rock';
 		console.log(userChoice);
@@ -21,19 +21,21 @@ $(document).ready(function(){
 		display();
 		compare(userChoice, computerChoice);
 	});
-	var computerChoice = Math.random();
-	if (computerChoice < 0.34) {
-		computerChoice = "rock";
-		$('#c_img').attr("src","stone.png");
-	} 
-	else if(computerChoice <= 0.67) {
-		computerChoice = "paper";
-		$('#c_img').attr("src","paper.png");
-	} 
-	else {
-		computerChoice = "scissor";
-		$('#c_img').attr("src","scissor.svg");
-	} 
+	var compChoice = function(){
+		computerChoice = Math.random();
+		if (computerChoice < 0.34) {
+			computerChoice = "rock";
+			$('#c_img').attr("src","stone.png");
+		} 
+		else if(computerChoice <= 0.67) {
+			computerChoice = "paper";
+			$('#c_img').attr("src","paper.png");
+		} 
+		else {
+			computerChoice = "scissor";
+			$('#c_img').attr("src","scissor.svg");
+		} 
+	};
 	console.log("Computer: " + computerChoice);
 	var compare = function(choice1, choice2){
 	    if(choice1===choice2)
@@ -127,7 +129,10 @@ $('#continue').click(function(){
 			pWrapper.toggleClass('col-lg-6 col-lg-12');
 			$('.scissor, .stone, .paper').toggleClass('col-lg-12 col-lg-4');
 		}
+		compChoice();
 })
 var width=$(window).width();
 console.log(width);
+
+	compChoice();
 })
