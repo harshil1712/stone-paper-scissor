909a187112756901d810b6dda73464dace4b9974
ghagrawal17@gmail.com <ghagrawal17@gmail.com>
2017-05-16 21:54:28 +0530

    ready

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
 $(document).ready(function(){
-	var userChoice,computerChoice;
+	var userChoice,computerChoice,playerCount=0,computerCount=0;
 	$('#stone').click(function(){
 		userChoice='rock';
 		console.log(userChoice);
+		$('#paper, #scissor').css('display','none');
+		$('.player').css('width','500px');
+		$('.computer').css('width','500px');
+		$('.stone').toggleClass('col-md-4 col-md-12');
+		$('#pWrapper').toggleClass('col-md-12 col-md-6');
+		$('.computer').css("display", "block");
 		compare(userChoice, computerChoice);
 	});
 	$('#paper').click(function(){
 		userChoice='paper';
 		console.log(userChoice);
+		$('#stone, #scissor').css('display','none');
+		$('.player').css('width','500px');
+		$('.computer').css('width','500px');
+		$('.paper').toggleClass('col-md-4 col-md-12');
+		$('#pWrapper').toggleClass('col-md-12 col-md-6');
+		$('.computer').css("display", "block");
 		compare(userChoice, computerChoice);
 	});
 	$('#scissor').click(function(){
 		userChoice='scissor';
 		console.log(userChoice);
+		$('#paper, #stone').css('display','none');
+		$('.player').css('width','500px');
+		$('.computer').css('width','500px');
+		$('.scissor').toggleClass('col-md-4 col-md-12');
+		$('#pWrapper').toggleClass('col-md-12 col-md-6');
+		$('.computer').css("display", "block");
 		compare(userChoice, computerChoice);
 	});
 	var computerChoice = Math.random();
 	if (computerChoice < 0.34) {
 		computerChoice = "rock";
+		$('#c_img').attr("src","stone.png");
 	} 
 	else if(computerChoice <= 0.67) {
 		computerChoice = "paper";
+		$('#c_img').attr("src","paper.png");
 	} 
 	else {
 		computerChoice = "scissors";
+		$('#c_img').attr("src","scissor.svg");
 	} 
 	console.log("Computer: " + computerChoice);
 	var compare = function(choice1, choice2){
@@ -35,34 +56,44 @@ $(document).ready(function(){
 	    {
 	        if(choice2==="scissors")
 	        {
-	            console.log("rock wins");
+	            console.log("Player Wins");
+	            playerCount++;
 	        }
 	        else
 	        {
-	            console.log("paper wins");
+	            console.log("Computer wins");
+	            computerCount++;
 	        }
 	    }
 	    else if(choice1==="paper")
 	    {
 	        if(choice2==="rock")
 	        {
-	            console.log("paper wins");
+	            console.log("Player wins");
+	            playerCount++;
 	        }
 	        else
 	        {
-	            console.log("scissors wins");
+	            console.log("Computer wins");
+	            computerCount++;
 	        }
 	    }
-	    else if(choice1==="scissors")
+	    else if(choice1==="scissor")
 	    {
-	        if(choice2==="rock")
+	        if(choice2==="paper")
 	        {
-	            console.log("rock wins");
+	            console.log("Player wins");
+	            playerCount++;
 	        }
 	        else
 	        {
-	            console.log("scissors wins");
+	            console.log("Computer wins");
+	            computerCount++;
 	        }
 	    }
+	    $('#playerScore').text(playerCount);
+		$('#computerScore').text(computerCount);
 	}
+	
+
 })
